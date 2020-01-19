81edb96270fd2167bbf84708802cc3da6cfc523c
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-01-19 20:59:06 +0900

    flask server上からblenderコマンドを実行可能にした

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 # coding:utf-8
 
 from flask import Flask
+import subprocess
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
+  res = subprocess.run(["/Applications/Blender.app/Contents/MacOS/Blender", "--background", "--python", "import_fbx_export_blend_file.py"], stdout=subprocess.PIPE)
+  print(res)
   return 'Hello, World!'
 
 if __name__ == "__main__":
