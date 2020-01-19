c79d9cb0fd1290d2ed426b3ccd5ca8bd99029006
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-01-19 19:28:42 +0900

    とりあえずスクショをレンダリング

--- render.py
+++ render.py
@@ -2,4 +2,4 @@ import bpy
 
 bpy.ops.render.render()
 bpy.data.images['Render Result'].save_render(
-    filepath='/Users/kobayashi/workspace/project/blender_server/image.png')
+    filepath='./image.png')
